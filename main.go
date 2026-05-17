180ad8fc1ef2f46cddbc8a7e6ff023d2b5f4cea6
Marcelo <marcelo.guzman.g@usach.cl>
2026-05-17 00:56:50 -0400

    feat: read operation

--- main.go
+++ main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
-	"fmt"
-	// "io"
 	"encoding/binary"
 	"errors"
+	"fmt"
+	"io"
 	"log"
 	"math"
 	"os"
 )
 
-func putKey(keyToByteOffset map[string]int, file *os.File, key, value string) error {
+var KEY_SIZE_BYTES = 2
+var VALUE_SIZE_BYTES = 4
+
+func putKey(keyToByteOffset map[string]int64, file *os.File, key, value string) error {
 	if len(key) > math.MaxUint16 {
 		return errors.New("key size exceeds maximum for 2^16 (65535 bytes)")
 	}
@@ -27,7 +30,53 @@ func putKey(keyToByteOffset map[string]int, file *os.File, key, value string) er
 	copy(buffer[6:keyEnd], key)
 	copy(buffer[keyEnd:valueEnd], value)
 
+	startPos, err := file.Seek(0, io.SeekEnd)
+	if err != nil {
+		log.Fatalf("Failed to seek to end of file: %v", err)
+	}
+
+	bytesWritten, err := file.Write(buffer)
+	if err != nil {
+		log.Fatal("Couldn't write to disk")
+	}
+	keyToByteOffset[key] = startPos
+	fmt.Printf("written: %d with startingPos: %d\n", bytesWritten, startPos)
 	return nil
+}
+
+func getKey(keyToByteOffset map[string]int64, file *os.File, key string) (string, error) {
+	val, ok := keyToByteOffset[key]
+	if !ok {
+		return "", errors.New("Value not in memory")
+	}
+
+	// whence -> from where do i start reading/writing bytes?
+	_, err := file.Seek(val, io.SeekStart)
+	if err != nil {
+		log.Fatalf("Failed to seek to offset: %v", err)
+	}
+
+	keySizeBuffer := make([]byte, KEY_SIZE_BYTES)
+	_, err = file.Read(keySizeBuffer)
+	if err != nil {
+		log.Fatalf("Failed to read key size: %v", err)
+	}
+
+	keySize := binary.BigEndian.Uint16(keySizeBuffer)
+
+	fmt.Printf("keySize=%d\n", keySize)
+
+	valueSizeBuffer := make([]byte, VALUE_SIZE_BYTES)
+	_, err = file.Read(valueSizeBuffer)
+	if err != nil {
+		log.Fatalf("Failed to read key size: %v", err)
+	}
+
+	valueSize := binary.BigEndian.Uint32(valueSizeBuffer)
+
+	fmt.Printf("valueSize=%d\n", valueSize)
+
+	return "", nil
 }
 
 func initFile(filePath string) {
@@ -36,17 +85,16 @@ func initFile(filePath string) {
 		log.Fatal(err)
 	}
 	defer file.Close()
-	file.WriteString("key,value\n")
 }
 
 func initMap() {
-	fmt.Println("Initialization of map with existing memory.csv file...")
+	fmt.Println("Initialization of map with existing memory.log file...")
 }
 
 func main() {
-	keyToByteOffset := make(map[string]int)
+	keyToByteOffset := make(map[string]int64)
 
-	filePath := "memory.csv"
+	filePath := "memory.log"
 	file, err := os.OpenFile(filePath, os.O_APPEND, 0644)
 	if err == nil {
 		initMap()
@@ -56,5 +104,7 @@ func main() {
 	}
 
 	file, err = os.OpenFile(filePath, os.O_APPEND, 0644)
-	putKey(keyToByteOffset, file, "10", "{ value1: 'value1', value2: 'value2}")
+	putKey(keyToByteOffset, file, "100", "{ value1: 'value1', value2: 'value2}")
+	val, err := getKey(keyToByteOffset, file, "100")
+	fmt.Printf("%s\n", val)
 }
