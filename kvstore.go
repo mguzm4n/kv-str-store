5eba0ae9b0b81519a8dd22402003873bc83c9719
Marcelo <marcelo.guzman.g@usach.cl>
2026-05-17 15:11:24 -0400

    feat: initialize mock put and get key methods

--- kvstore.go
+++ kvstore.go
@@ -1,27 +1,65 @@
 package main
 
+import (
+	"errors"
+	"os"
+	"sync"
+)
+
 type Segment struct {
+	mu             sync.RWMutex
 	keyToOffsetMap map[string]uint64
-	fd             uint64
+	file           *os.File
 }
 
 type Store struct {
-	Segments      []Segment
+	mu            sync.Mutex
+	Segments      []*Segment
 	ActiveSegment *Segment
 }
 
-func New() *Store {
-	return &Store{}
+func newSegment(filename string) (*Segment, error) {
+	segment := &Segment{
+		keyToOffsetMap: make(map[string]uint64),
+	}
+	f, err := os.OpenFile(filename, os.O_CREATE, 0644)
+	if err != nil {
+		return nil, errors.New("Couldn't create new segment")
+	}
+	segment.file = f
+	return segment, nil
 }
 
-func orchestrator() {
+func New() (*Store, error) {
+	segments := make([]*Segment, 0)
+	// TODO: recover state from disk
 
+	active, err := newSegment("active")
+	if err != nil {
+		return nil, errors.New("Couldn't bootstrap the store")
+	}
+
+	store := &Store{
+		Segments:      segments,
+		ActiveSegment: active,
+	}
+	go store.compact()
+	return store, nil
 }
 
-func writer() {
-
+func (s *Store) PutKey(key, value string) {
+	activeSegment := s.ActiveSegment
+	activeSegment.mu.Lock()
+	defer activeSegment.mu.Unlock()
 }
 
-func compact() {
+func (s *Store) GetKey(key string) (string, error) {
+	activeSegment := s.ActiveSegment
+	activeSegment.mu.Lock()
+	defer activeSegment.mu.Unlock()
+	return "", nil
+}
+
+func (s *Store) compact() {
 
 }
